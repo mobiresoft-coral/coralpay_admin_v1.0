61d4343d42e6bc39e5edac2438b543d65baf61cd
hadigun <adigun.hmd@gmail.com>
2025-10-06 12:36:29 +0100

    Remove .next 06102025

--- .next/static/chunks/app_(dashboard)_users_page_tsx_2354acd9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_5eed3eee._.js",
-  "static/chunks/node_modules__pnpm_0880d4dc._.js"
-],
-    source: "dynamic"
-});
