86285359b067844383044c3ae4b4539f60b8ab33
hadigun <hadigun@gmail.com>
2025-10-09 13:42:13 +0100

    latest

--- lib/plugin-utils.ts
+++ lib/plugin-utils.ts
@@ -1,134 +1,143 @@
-import { INPUT_TOOL_TYPES, PLUGIN_TOOL_TYPES } from "@/constants/tools"
-import type { ToolType, PluginToolType, InputToolType, NodeType, Plugin, NodeData } from "@/types"
+import { INPUT_TOOL_TYPES, PLUGIN_TOOL_TYPES } from "@/constants/tools";
+import type {
+  InputToolType,
+  NodeData,
+  NodeType,
+  Plugin,
+  PluginToolType,
+  ToolType,
+} from "@/types";
 
 import type {
-	ApiCallPlugin,
-	PaymentPlugin,
-	ValidationPlugin,
-	RouterPlugin,
-} from "@mobiresoft-coral/ussd-shared-core"
+  ApiCallPlugin,
+  PaymentPlugin,
+  RouterPlugin,
+  ValidationPlugin,
+} from "@mobiresoft-coral/ussd-shared-core";
 
-import { getId } from "./utils"
+import { getId } from "./utils";
 
 export function createApiPlugin(): ApiCallPlugin {
-	return {
-		id: crypto.randomUUID(),
-		toolType: "plugin_apicall",
-		type: "api_call",
-		order: 0,
-		config: {
-			url: "",
-			method: "GET",
-			headers: {},
-			queryParams: {},
-		},
-	}
+  return {
+    id: crypto.randomUUID(),
+    toolType: "plugin_apicall",
+    type: "api_call",
+    // order: 0,
+    config: {
+      url: "",
+      method: "GET",
+      headers: {},
+      queryParams: {},
+    },
+  };
 }
 
 export function createPaymentPlugin(): PaymentPlugin {
-	return {
-		id: crypto.randomUUID(),
-		toolType: "plugin_payment",
-		type: "payment",
-		order: 0,
-		config: {
-			endTemplate: "",
-			amount: "",
-		},
-	}
+  return {
+    id: crypto.randomUUID(),
+    toolType: "plugin_payment",
+    type: "payment",
+    // order: 0,
+    config: {
+      endTemplate: "",
+      amount: "",
+    },
+  };
 }
 
 export function createRouterPlugin(): RouterPlugin {
-	return {
-		id: crypto.randomUUID(),
-		toolType: "plugin_router",
-		type: "router",
-		order: 0,
-		config: {
-			routes: [{ id: getId(), condition: "", input: "" }],
-			defaultRoute: "",
-			errorRoute: "",
-		},
-	}
+  return {
+    id: crypto.randomUUID(),
+    toolType: "plugin_router",
+    type: "router",
+    // order: 0,
+    config: {
+      routes: [{ id: getId(), condition: "", input: "" }],
+      defaultRoute: "",
+      errorRoute: "",
+    },
+  };
 }
 export function createValidationPlugin(
-	toolType: ToolType,
-	regexp: string = "",
-	isNumeric: boolean = false
+  toolType: ToolType,
+  regexp: string = "",
+  isNumeric: boolean = false
 ): ValidationPlugin {
-	return {
-		id: crypto.randomUUID(),
-		toolType,
-		type: "validation",
-		order: 0,
-		config: {
-			inputKey: "",
-			regexp,
-			isNumeric,
-			outputKey: "",
-			message: "Invalid input",
-		},
-	}
+  return {
+    id: crypto.randomUUID(),
+    toolType,
+    type: "validation",
+    // order: 0,
+    config: {
+      inputKey: "",
+      regexp,
+      isNumeric,
+      outputKey: "",
+      message: "Invalid input",
+    },
+  };
 }
 
 export function createEmailInputPlugin(): ValidationPlugin {
-	return createValidationPlugin(
-		"input_email",
-		"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-		false
-	)
+  return createValidationPlugin(
+    "input_email",
+    "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+    false
+  );
 }
 
 export function createPhoneInputPlugin(): ValidationPlugin {
-	return createValidationPlugin("input_phone", "^[0-9]{10}$", true)
+  return createValidationPlugin("input_phone", "^[0-9]{10}$", true);
 }
 
 export function createWebsiteInputPlugin(): ValidationPlugin {
-	return createValidationPlugin(
-		"input_website",
-		"^(https?:\\/\\/)?([\\w.-]+)\\.([a-z]{2,})([\\/\\w.-]*)*\\/?$",
-		false
-	)
+  return createValidationPlugin(
+    "input_website",
+    "^(https?:\\/\\/)?([\\w.-]+)\\.([a-z]{2,})([\\/\\w.-]*)*\\/?$",
+    false
+  );
 }
 
 export function createNumberInputPlugin(): ValidationPlugin {
-	return createValidationPlugin("input_number", "^[0-9]+$", true)
+  return createValidationPlugin("input_number", "^[0-9]+$", true);
 }
 
 export function createTextInputPlugin(): ValidationPlugin {
-	return createValidationPlugin("input_text", ".*", false)
+  return createValidationPlugin("input_text", ".*", false);
 }
 
-export function isPluginOrInputTool(tool: unknown): tool is PluginToolType | InputToolType {
-	return (
-		PLUGIN_TOOL_TYPES.includes(tool as PluginToolType) ||
-		INPUT_TOOL_TYPES.includes(tool as InputToolType)
-	)
+export function isPluginOrInputTool(
+  tool: unknown
+): tool is PluginToolType | InputToolType {
+  return (
+    PLUGIN_TOOL_TYPES.includes(tool as PluginToolType) ||
+    INPUT_TOOL_TYPES.includes(tool as InputToolType)
+  );
 }
 
 interface CreateNodeDataParams {
-	name: string
-	type: NodeType
-	postPlugins?: Plugin[]
-	prePlugins?: Plugin[]
-	supportsPlugins?: boolean
-	renderTemplate?: string
+  name: string;
+  type: NodeType;
+  postPlugins?: Plugin[];
+  prePlugins?: Plugin[];
+  supportsPlugins?: boolean;
+  renderTemplate?: string;
 }
 
 export function createNodeData({
-	name,
-	type,
-	postPlugins = [],
-	prePlugins = [],
-	renderTemplate = "",
+  name,
+  type,
+  postPlugins = [],
+  prePlugins = [],
+  renderTemplate = "",
 }: CreateNodeDataParams): NodeData {
-	return {
-		name,
-		type,
-		postPlugins,
-		prePlugins,
-		renderTemplate,
-		id: getId(),
-		meta: {},
-	}
+  return {
+    name,
+    type,
+    postPlugins,
+    prePlugins,
+    renderTemplate,
+    id: getId(),
+    meta: {},
+  };
 }
